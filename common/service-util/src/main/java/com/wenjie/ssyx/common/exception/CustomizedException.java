a63b365d7425c8af765894adde14a2dad3316247
Wenjie FU <wenjie.fu@imt-atlantique.net>
2024-01-14 09:48:22 +0100

    add model and exception handler

---
+++ common/service-util/src/main/java/com/wenjie/ssyx/common/exception/CustomizedException.java
@@ -0,0 +1,27 @@
+package com.wenjie.ssyx.common.exception;
+
+import com.wenjie.ssyx.common.result.ResultCodeEnum;
+import lombok.Data;
+
+/**
+ * @className: CustomizedExcpetion
+ * @description: TODO
+ * @author: Wenjie FU
+ * @date: 14/01/2024
+ **/
+
+@Data
+public class CustomizedException extends RuntimeException {
+  private Integer code;
+  private String message;
+
+  public CustomizedException(Integer code, String message) {
+    super(message);
+    this.code = code;
+  }
+
+  public CustomizedException(ResultCodeEnum resultCodeEnum) {
+    super(resultCodeEnum.getMessage());
+    this.code = resultCodeEnum.getCode();
+  }
+}
---
+++ common/service-util/src/main/java/com/wenjie/ssyx/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.wenjie.ssyx.common.exception;
+
+import com.wenjie.ssyx.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @className: GloabalExceptionHandler
+ * @description: TODO
+ * @author: Wenjie FU
+ * @date: 14/01/2024
+ **/
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(Exception.class)
+  @ResponseBody
+  public Result error(Exception e) {
+    e.printStackTrace();
+    return Result.fail(null);
+  }
+
+  @ExceptionHandler(Exception.class)
+  @ResponseBody
+  public Result error(CustomizedException e) {
+    e.printStackTrace();
+    return Result.fail(null);
+  }
+}
