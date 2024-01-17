3ae6d49e83ddc753f1c83b0499917636797fce08
Wenjie FU <wenjie.fu@imt-atlantique.net>
2024-01-17 18:40:33 +0100

    create package service-acl and add SpringBootApplication boot code

---
+++ service/service-acl/src/main/java/com/wenjie/ssyx/ServiceAclApplication.java
@@ -0,0 +1,17 @@
+package com.wenjie.ssyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @className: ServiceAclApplication - Acl stands for Access Control List
+ * @description: TODO
+ * @author: Wenjie FU
+ * @date: 17/01/2024
+ **/
+@SpringBootApplication
+public class ServiceAclApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceAclApplication.class, args);
+  }
+}
